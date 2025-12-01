edf011ed225f85277e2a53508478c4af1b7729c6
Renato Oliveira <roliveira3891@gmail.com>
2025-12-01 20:25:36 -0300

    atualização

--- apps/web/modules/integrations/constants.ts
+++ apps/web/modules/integrations/constants.ts
@@ -23,7 +23,7 @@ export const INTEGRATIONS = [
 
 export type IntegrationId = (typeof INTEGRATIONS)[number]["id"];
 
-export const HTML_SCRIPT = `<script src="https://next15-echo-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const REACT_SCRIPT = `<script src="https://next15-echo-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const NEXTJS_SCRIPT = `<script src="https://next15-echo-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
-export const JAVASCRIPT_SCRIPT = `<script src="https://next15-echo-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const HTML_SCRIPT = `<script src="https://echo-ia-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const REACT_SCRIPT = `<script src="https://echo-ia-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const NEXTJS_SCRIPT = `<script src="https://echo-ia-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
+export const JAVASCRIPT_SCRIPT = `<script src="https://echo-ia-widget.vercel.app/widget.js" data-organization-id="{{ORGANIZATION_ID}}"></script>`;
